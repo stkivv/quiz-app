81e52c2a5805ab4ea8307accd6c8a171dc0a2710
Stefan Kivvistik <stefankivv@gmail.com>
2024-12-28 13:11:22 +0200

    docker setup

--- backend/src/main/java/com/stkivv/webquiz/backend/domain/AppUser.java
+++ backend/src/main/java/com/stkivv/webquiz/backend/domain/AppUser.java
@@ -2,7 +2,6 @@ package com.stkivv.webquiz.backend.domain;
 
 import java.util.Collection;
 import java.util.List;
-import java.util.Set;
 import java.util.UUID;
 import java.util.ArrayList;
 
@@ -17,67 +16,73 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.Size;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor
 public class AppUser implements UserDetails {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID userId;
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private UUID userId;
 
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "appUser")
-    private List<Quiz> quizzes;
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "appUser")
+	@Size(max = 400)
+	private List<Quiz> quizzes;
 
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "appUser")
-    private List<RefreshToken> refreshTokens;
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "appUser")
+	private List<RefreshToken> refreshTokens;
 
-    @Column(nullable = false)
-    private String username;
-    @Column(nullable = false)
-    private String password;
-    private boolean isEnabled;
+	@Column(nullable = false)
+	@Size(min = 4, max = 30)
+	private String username;
 
-    public AppUser(String username, String password) {
-        this.username = username;
-        this.password = password;
-        isEnabled = true;
-    }
+	@Column(nullable = false)
+	@Size(min = 7, max = 30)
+	private String password;
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return new ArrayList<SimpleGrantedAuthority>();
-    }
+	private boolean isEnabled;
 
-    @Override
-    public String getPassword() {
-        return password;
-    }
+	public AppUser(String username, String password) {
+		this.username = username;
+		this.password = password;
+		isEnabled = true;
+	}
 
-    @Override
-    public String getUsername() {
-        return username;
-    }
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return new ArrayList<SimpleGrantedAuthority>();
+	}
 
-    @Override
-    public boolean isAccountNonExpired() {
-        return isEnabled;
-    }
+	@Override
+	public String getPassword() {
+		return password;
+	}
 
-    @Override
-    public boolean isAccountNonLocked() {
-        return isEnabled;
-    }
+	@Override
+	public String getUsername() {
+		return username;
+	}
 
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return isEnabled;
-    }
+	@Override
+	public boolean isAccountNonExpired() {
+		return isEnabled;
+	}
 
-    @Override
-    public boolean isEnabled() {
-        return isEnabled;
-    }
+	@Override
+	public boolean isAccountNonLocked() {
+		return isEnabled;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return isEnabled;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return isEnabled;
+	}
 
 }
